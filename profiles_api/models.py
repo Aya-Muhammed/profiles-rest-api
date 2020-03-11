9bae3d76313e122f4c74f3e8d6a9c943af9e8b99
Aya <sasukenrt520@gmail.com>
2020-03-11 14:32:43 +0200

    Enable Django admin for user profile model

--- profiles_api/models.py
+++ profiles_api/models.py
@@ -22,7 +22,7 @@ class UserProfileManger(BaseUserManager):
 
     def create_superuser(self, email, name, password):
         """Create and save a new superuser with given details"""
-        user = self.create_user(self, email, name, password)
+        user = self.create_user(email, name, password)
 
         user.is_superuser = True
         user.is_staff = True
